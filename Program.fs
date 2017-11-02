9df1655e8796082d581ecde1d2895f4ff0656e56
michel nielsen <michel@randahl.net>
2017-11-03 00:37:00 +0100

    playing around with different types of parsing

--- Program.fs
+++ Program.fs
@@ -4,10 +4,9 @@ open System
 open System.Net.Sockets
 open System.IO
 open System.Text
-// TODO: are all these imports needed? and does the order of imports matter?
 open Utf8Json
-open Utf8Json.Resolvers
-open Utf8Json.FSharp
+//open Utf8Json.Resolvers
+//open Utf8Json.FSharp
 open MessagePack
 open MessagePack.Resolvers
 open MessagePack.FSharp
@@ -29,14 +28,26 @@ let send stuff =
     let bytes = MessagePackSerializer.Serialize(stuff)
     writer.Write(bytes)
 
-let receive _ =
-    let bytes: byte[] = Array.create my_sock.ReceiveBufferSize (byte(0))
+let receive _ : byte [] =
+    let bytes: byte [] = Array.create my_sock.ReceiveBufferSize (byte(0))
     reader.Read (bytes, 0, int my_sock.ReceiveBufferSize) |> ignore
-    MessagePackSerializer.ToJson(bytes)
+    bytes
+
+let parser1 (bytes : byte []) = MessagePackSerializer.ToJson(bytes)
+
+[<MessagePackObject(true)>]
+type MyType<'t1,'t2,'t3,'t4> =
+    { version: 't1; functions: 't2; error_types: 't3; types: 't4 }
+
+let parser2 (bytes : byte []) = MessagePackSerializer.Deserialize<Tuple<_,_,_,Tuple<_, MyType<_,_,_,_>>>>(bytes)
+
 
 [<EntryPoint>]
 let main argv =
     send (0, 1337, "vim_get_api_info", [||]) |> ignore
-    receive ()
-    |> printfn "%s"
+    let bytes = receive ()
+    bytes
+    |> parser1
+    |> printfn "%A"
+
     0 // return an integer exit code
